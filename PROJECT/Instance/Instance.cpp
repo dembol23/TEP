c7974381836e61511b298863b7a0acb7164d0c8b
dembol23 <wdembny.2005@gmail.com>
2025-12-22 23:20:41 +0100

    Added mutation and crossing methods

--- PROJECT/Instance/Instance.cpp
+++ PROJECT/Instance/Instance.cpp
@@ -2,12 +2,44 @@
 
 #include <random>
 
+Instance::Instance(const std::vector<int> &genotype):genotype(genotype){};
+
 Instance::Instance(const int nodes, const int number_of_groups) {
-    std::random_device dev;
-    std::mt19937 rng(dev());
+    static std::random_device dev;
+    static std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist(0, number_of_groups - 1);
     for (int i = 1; i < nodes; i++) {
         genotype.push_back(dist(rng));
     }
 }
 
+std::vector<int> Instance::mutate(const double mutate_prob, const int number_of_groups) {
+    static std::random_device dev;
+    static std::mt19937 rng(dev());
+
+    std::uniform_real_distribution<> dist_prob(0.0, 1.0);
+    std::uniform_int_distribution<> dist_truck(0, number_of_groups - 1);
+
+    for (int & i : genotype) {
+        if (dist_prob(rng) < mutate_prob) {
+            i = dist_truck(rng);
+        }
+    }
+    return genotype;
+}
+
+std::pair<SmartPointer<Instance>, SmartPointer<Instance>> Instance::cross(Instance* other, double cross_prob) {
+    static std::random_device dev;
+    static std::mt19937 rng(dev());
+    std::uniform_real_distribution<> dist_prob(0.0, 1.0);
+    SmartPointer<Instance> child_1(new Instance(genotype));
+    SmartPointer<Instance> child_2(new Instance(other->genotype));
+    if (dist_prob(rng) < cross_prob) {
+        std::uniform_int_distribution<std::mt19937::result_type> dist(0, genotype.size() - 1);
+        int split_index = dist(rng);
+        for (int i = split_index; i < genotype.size(); i++) {
+            std::swap(child_1->genotype[i], child_2->genotype[i]);
+        }
+    }
+    return std::make_pair(child_1,child_2);
+}
--- PROJECT/Instance/Instance.h
+++ PROJECT/Instance/Instance.h
@@ -6,9 +6,9 @@
 class Instance {
 public:
     explicit Instance(int nodes, int number_of_groups);
-    explicit Instance(std::vector<int> genotype);
+    explicit Instance(const std::vector<int> &genotype);
     double calcFitness();
-    std::vector<int> mutate(double mutate_prob);
+    std::vector<int> mutate(double mutate_prob,int number_of_groups);
     std::pair<SmartPointer<Instance>, SmartPointer<Instance>> cross(Instance* other, double cross_prob);
     std::vector<int> getGenotype() {return genotype;};
 private:
