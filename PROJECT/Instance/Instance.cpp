aaa10d8ba788a92b47aa75d417748c10c26aadc5
dembol23 <wdembny.2005@gmail.com>
2025-12-22 22:23:37 +0100

    quick fix

--- PROJECT/Instance/Instance.cpp
+++ PROJECT/Instance/Instance.cpp
@@ -3,10 +3,10 @@
 #include <random>
 
 Instance::Instance(const int nodes, const int number_of_groups) {
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(0, number_of_groups - 1);
     for (int i = 1; i < nodes; i++) {
-        std::random_device dev;
-        std::mt19937 rng(dev());
-        std::uniform_int_distribution<std::mt19937::result_type> dist(1,number_of_groups);
         genotype.push_back(dist(rng));
     }
 }
